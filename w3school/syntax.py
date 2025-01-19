c3b5fe693518d962add6aef47dba9ebeed308153
Alex Rakusevich <a_rakusevich@kbtu.kz>
2025-01-19 19:24:59 +0500

    Add examples from HOME section of LAB1

---
+++ w3school/syntax.py
@@ -0,0 +1,6 @@
+if 6 > 1:
+  print("Six is greater than one!")
+#This is a comment.
+
+x = 5
+y = "Hello, World!"
---
+++ w3school/variables.py
@@ -0,0 +1,16 @@
+x = 5
+y = "Alex"
+print(x)
+print(y)
+#variables names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+#assign multiple values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
