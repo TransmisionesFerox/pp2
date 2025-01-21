b40055ba28927e1592f5678834717489387da5a0
Alex Rakusevich <a_rakusevich@kbtu.kz>
2025-01-21 14:41:27 +0500

    Lab2

---
+++ lab2/booleans.py
@@ -0,0 +1,27 @@
+a = 100
+b = 22
+
+if b > a:
+    print ("b is greater than a")
+else:
+    print ("b is not greater than a")
+
+#exercises
+#1
+print(5 > 3)
+#true
+#2
+print(10 > 9)
+#True
+#3
+print(10 == 9)
+#false
+#4
+print(10 < 9)
+#false
+#5
+print(bool("abc"))
+#True
+#6
+print(bool(0))
+#false
---
+++ lab2/operators.py
@@ -0,0 +1,26 @@
+print (10+4)
+print (8-6)
+print (6*2)
+print (5%2)
+print((5 + 2) - (5 + 2))
+
+#exercises
+#1
+x = 5
+x += 3
+print(x)
+#x = 8
+#2
+print(10*5)
+#3
+print(10/2)
+#4
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+#5
+if 5 != 10:
+  print("5 and 10 is not equal")
+#6
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
